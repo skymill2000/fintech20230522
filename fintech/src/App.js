ffdb07074c3a4ca2c6326e3a722f53092aa60c74
유관우 <skymill2000@gmail.com>
2023-05-23 16:35:23 +0900

    lecture(props): 프롭스를 통해 컴포넌트에 데이터 전달

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,18 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const Welcome = () => {
-  return <p>처음으로 만든 컴포넌트 입니다.</p>;
+const Welcome = ({ username }) => {
+  console.log(username);
+  return <p>안녕하세요! {username}님 반값습니다.</p>;
 };
 
 function App() {
   return (
     <div className="App">
-      <Welcome></Welcome>
-      <Welcome></Welcome>
-      <Welcome></Welcome>
-      <Welcome></Welcome>
-      <Welcome></Welcome>
+      <Welcome username="홍길동"></Welcome>
+      <Welcome username="고길동"></Welcome>
     </div>
   );
 }
