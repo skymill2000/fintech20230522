7329a70ec2f0c5e1c3b585f831427d0c5c2b425d
유관우 <skymill2000@gmail.com>
2023-05-23 15:33:08 +0900

    lecture(구조분해할당): 구조분해 할당 실습

--- js_basic/example04.js
+++ js_basic/example04.js
@@ -8,13 +8,13 @@ console.log(array);
 console.log(array[0]);
 console.log(array[4]);
 
-// for (let i = 0; i < array.length; i++) {
-//   console.log(array[i]); //0,1,2,3,4,5
-// }
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]); //0,1,2,3,4,5
+}
 
-// for (arr of array) {
-//   console.log(arr);
-// }
+for (arr of array) {
+  console.log(arr);
+}
 
 array.map((arr) => {
   console.log(arr);
---
+++ js_basic/example05.js
@@ -0,0 +1,13 @@
+const car = {
+  name: "sonata",
+  ph: 149,
+  maker: "hyundai",
+};
+
+// let name = car.name;
+// let ph = car.ph;
+// let make = car.make;
+
+// console.log(name, ph);
+const { name, ph, maker } = car;
+console.log(name, ph, maker);
