3f099a6f2652804a177ea19650a4cbe54213e47e
유관우 <skymill2000@gmail.com>
2023-05-23 15:25:42 +0900

    lecture(반복문): 배열과 반복문 실습

--- js_basic/example04.js
+++ js_basic/example04.js
@@ -7,3 +7,15 @@ let array = [1, 2, 3, 4, "String", 43.923];
 console.log(array);
 console.log(array[0]);
 console.log(array[4]);
+
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]); //0,1,2,3,4,5
+// }
+
+// for (arr of array) {
+//   console.log(arr);
+// }
+
+array.map((arr) => {
+  console.log(arr);
+});
