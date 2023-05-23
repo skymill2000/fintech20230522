6cb6d4f9fc25b9792e38d52b265e47e70f8da97e
유관우 <skymill2000@gmail.com>
2023-05-23 15:39:24 +0900

    lecture(템플릿리터럴): 템플릿 리터럴 사용 예제 실습

---
+++ js_basic/example06.js
@@ -0,0 +1,7 @@
+let name = "홍길동";
+let age = 33;
+let text = "안녕하세요 : " + name + "님 반갑습니다!" + age + " 세 입니다.";
+let template = `안녕하세요 ! ${name}님 반갑습니다. ${age} 세 이내요`;
+
+console.log(text);
+console.log(template);
