d5d773c8ae8617f7d8c6f1a33c2b236f319ce974
유관우 <skymill2000@gmail.com>
2023-05-24 10:35:36 +0900

    lecture(리스트컴포넌트): 리스트 컴포넌트 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Welcome from "./components/Welcome";
 import StateComponent from "./components/StateComponent";
 import InputComponents from "./components/InputComponents";
+import ListComponents from "./components/ListComponents";
 
 function App() {
   return (
     <div className="App">
       <InputComponents></InputComponents>
+      <ListComponents></ListComponents>
     </div>
   );
 }
--- fintech/src/components/ListComponents.js
+++ fintech/src/components/ListComponents.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ListComponents = () => {
   const [list, setList] = useState([
@@ -7,7 +7,19 @@ const ListComponents = () => {
     { username: "둘리", age: 23, height: 133 },
   ]);
 
-  return <div></div>;
+  return (
+    <div>
+      {list.map((el) => {
+        return (
+          <>
+            <p>이름 : {el.username}</p>
+            <p>나이 : {el.age}</p>
+            <p>회원키 : {el.height}</p>
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ListComponents;
