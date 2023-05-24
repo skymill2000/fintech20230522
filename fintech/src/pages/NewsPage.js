e100caacc2727e48c32d20175579331ec7f8211e
유관우 <skymill2000@gmail.com>
2023-05-24 13:52:26 +0900

    lecture(뉴스API): 이벤트 바인딩, 상태 추가

--- fintech/src/pages/NewsPage.js
+++ fintech/src/pages/NewsPage.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderTitle from "../components/News/HeaderTitle";
 import SearchInput from "../components/News/SearchInput";
 import NewsList from "../components/News/NewsList";
 
 const NewsPage = () => {
+  const [searchInput, setSearchInput] = useState("검색어");
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setSearchInput(value);
+  };
+
+  const handleClick = () => {
+    console.log(searchInput);
+  };
+
   return (
     <div>
       <HeaderTitle title={"뉴스검색"}></HeaderTitle>
-      <SearchInput></SearchInput>
+      <SearchInput
+        handleChange={handleChange}
+        handleClick={handleClick}
+      ></SearchInput>
       <NewsList></NewsList>
     </div>
   );
