f6f130e3ecd179524d3016a7e0ac46a44df6ebe1
유관우 <skymill2000@gmail.com>
2023-05-25 10:15:37 +0900

    lecture(인증페이지): 인증페이지 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,22 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Welcome from "./components/Welcome";
-import StateComponent from "./components/StateComponent";
-import InputComponents from "./components/InputComponents";
-import ListComponents from "./components/ListComponents";
-import StyledComponents from "./components/StyledComponents";
-import AxiosComponents from "./components/AxiosComponents";
 import NewsPage from "./pages/NewsPage";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Welcome />}></Route>
-        <Route path="/state" element={<StateComponent />}></Route>
-        <Route path="/input" element={<InputComponents />}></Route>
-        <Route path="/axios" element={<AxiosComponents />}></Route>
+        <Route path="/" element={<AuthPage />}></Route>
         <Route path="/news" element={<NewsPage />}></Route>
       </Routes>
     </BrowserRouter>
