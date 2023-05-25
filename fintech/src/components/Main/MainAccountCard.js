d638c67718a6c02bd8fa4485f2dbdeb9647d0295
유관우 <skymill2000@gmail.com>
2023-05-25 11:58:12 +0900

    lecture(디자인변경): gpt 를통한 디자인 변경

--- fintech/src/components/Main/MainAccountCard.js
+++ fintech/src/components/Main/MainAccountCard.js
@@ -6,37 +6,60 @@ const CardBlock = styled.div`
   flex-direction: column;
   margin: 0.5rem;
   padding: 20px;
-  border: 1px #dfdfdf solid;
+  border: 1px solid #dfdfdf;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
 const CardTitle = styled.div`
-  font-size: 1rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-bottom: 10px;
 `;
+
 const FintechUseNo = styled.div`
-  font-size: 0.7rem;
-  margin-bottom: 30px;
+  font-size: 0.9rem;
+  color: #666;
+  margin-bottom: 20px;
 `;
 
 const QrButton = styled.button`
   width: 120px;
   height: 40px;
-  background: #2959d4;
+  background-color: #2959d4;
   color: white;
   border: none;
+  border-radius: 5px;
+  font-size: 0.9rem;
+  cursor: pointer;
+  outline: none;
+
+  &:hover {
+    background-color: #21428f;
+  }
 `;
 
 const BalanceButton = styled.button`
   width: 120px;
   height: 40px;
-  background: #df6a38;
+  background-color: #df6a38;
   color: white;
   margin-left: 10px;
   border: none;
+  border-radius: 5px;
+  font-size: 0.9rem;
+  cursor: pointer;
+  outline: none;
+
+  &:hover {
+    background-color: #ae4a23;
+  }
 `;
 
 const ButtonBlock = styled.div`
   display: flex;
-  flex-direction: row;
+  justify-content: flex-end;
+  margin-top: 20px;
 `;
 
 const MainAccountCard = ({ bankName, fintechUseNo }) => {
@@ -44,17 +67,19 @@ const MainAccountCard = ({ bankName, fintechUseNo }) => {
     console.log("QR 버튼 클릭");
     window.location.href = `/qrcode?fintechUseNo=${fintechUseNo}`;
   };
+
   const handleBalanceButton = () => {
     console.log("잔액조회 버튼 클릭");
     window.location.href = `/balance?fintechUseNo=${fintechUseNo}`;
   };
+
   return (
     <CardBlock>
       <CardTitle>{bankName}</CardTitle>
       <FintechUseNo>{fintechUseNo}</FintechUseNo>
       <ButtonBlock>
-        <QrButton onClick={handleQrButton}>qr코드</QrButton>
-        <BalanceButton onClick={handleBalanceButton}>잔액조회</BalanceButton>
+        <QrButton onClick={handleQrButton}>QR 코드</QrButton>
+        <BalanceButton onClick={handleBalanceButton}>잔액 조회</BalanceButton>
       </ButtonBlock>
     </CardBlock>
   );
