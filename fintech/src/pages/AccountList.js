76b53fb8cd32b4976c3328d0b02b9fe5529beae0
유관우 <skymill2000@gmail.com>
2023-05-25 11:41:30 +0900

    lecture(계좌목록조회): 계좌목록 조회 API 작성완료

--- fintech/src/pages/AccountList.js
+++ fintech/src/pages/AccountList.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HeaderTitle from "../components/News/HeaderTitle";
+import axios from "axios";
 
 const AccountList = () => {
+  let accessToken = "";
+  let userSeqNo = "";
+
+  useEffect(() => {
+    console.log(localStorage.getItem("accessToken"));
+    console.log(localStorage.getItem("userSeqNo"));
+    accessToken = localStorage.getItem("accessToken");
+    userSeqNo = localStorage.getItem("userSeqNo");
+    getAccountList();
+  }, []);
+
+  const getAccountList = () => {
+    const sendData = {
+      user_seq_no: userSeqNo,
+    };
+
+    const option = {
+      method: "GET",
+      url: "/v2.0/user/me",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: sendData,
+    };
+
+    axios(option).then(({ data }) => {
+      console.log(data);
+    });
+  };
+
   return (
     <div>
       <HeaderTitle title={"계좌목록"}></HeaderTitle>
