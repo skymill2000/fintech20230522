75d08ee43d9e1bf45a707824acad6de2806fc010
유관우 <skymill2000@gmail.com>
2023-05-26 11:26:00 +0900

    lecture(잔액조회): 잔액조회 기능 완료

--- fintech/src/pages/BalancePage.js
+++ fintech/src/pages/BalancePage.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import HeaderTitle from "../components/News/HeaderTitle";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 import axios from "axios";
+import BalanceCard from "../components/Balance/BalanceCard";
 
 const BalancePage = () => {
   const queryParams = useLocation().search;
   const fintechUseNo = queryString.parse(queryParams).fintechUseNo;
+  const [balance, setBalance] = useState("0");
 
   let accessToken = "";
   useEffect(() => {
@@ -16,28 +18,45 @@ const BalancePage = () => {
     getAccountBalance();
   }, []);
 
+  const genTransId = () => {
+    let countnum = Math.floor(Math.random() * 1000000000) + 1;
+    const clientNo = "M202300440"; //이용기관번호 본인것 입력
+    let transId = clientNo + "U" + countnum;
+    return transId;
+  };
+
   const getAccountBalance = () => {
     //해당 부분을 계좌 잔액 조회 API 요청으로 변경 필요
+    const sendObj = {
+      bank_tran_id: genTransId(),
+      fintech_use_num: fintechUseNo,
+      tran_dtime: "20230526100600",
+    };
 
     const option = {
       method: "GET",
-      url: "",
+      url: "v2.0/account/balance/fin_num",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         Authorization: `Bearer ${accessToken}`,
       },
-      params: {},
+      params: sendObj,
     };
 
     axios(option).then(({ data }) => {
       console.log(data);
-      //시뮬레이터 응답 전문 없음 (성공)
+      setBalance(data);
     });
   };
 
   return (
     <div>
       <HeaderTitle title={"잔액조회"}></HeaderTitle>
+      <BalanceCard
+        bankName={balance.bank_name}
+        fintechNo={balance.fintech_use_num}
+        balance={balance.balance_amt}
+      ></BalanceCard>
     </div>
   );
 };
